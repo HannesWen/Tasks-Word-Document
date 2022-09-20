06061dc62e39a1f01d7f6b6ecc5b890a17e74eeb
Wengert, Johannes <johannes.wengert@tecalliance.net>
2022-09-20 09:49:03 +0200

    //Seite 1, Aufgabe 2: Frage den Nutzer welche zwei Zahlen er addieren möchte und gebe am Ende das Ergebnis aus

--- Tasks Word Document/Tasks Word Document/Program.cs
+++ Tasks Word Document/Tasks Word Document/Program.cs
@@ -11,12 +11,18 @@ namespace Tasks_Word_Document
     {
         static void Main(string[] args)
         {
-            //o	Aufgabe 1: initialisiere 2 Zahlen und gebe die Summe der Zahlen auf der Console aus
-            int zahl1 = 1;
-            int zahl2 = 2;
-            Console.WriteLine($"Die Summe der Zahlen ist {zahl1 + zahl2}");
-            Console.ReadKey();
+            //Seite 1, Aufgabe 1: initialisiere 2 Zahlen und gebe die Summe der Zahlen auf der Console aus
+            //int zahl1 = 1;
+            //int zahl2 = 2;
+            //Console.WriteLine($"Die Summe der Zahlen ist {zahl1 + zahl2}");
+            //Console.ReadKey();
 
+            //Seite 1, Aufgabe 2: Frage den Nutzer welche zwei Zahlen er addieren möchte und gebe am Ende das Ergebnis aus
+            Console.WriteLine("Welche 2 Zahlen möchtest du summieren?");
+            int zahl1 = Convert.ToInt32(Console.ReadLine());
+            int zahl2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Die Summe der beiden Zahlen ist {zahl1 + zahl2}");
+            Console.ReadKey();
 
 
         }
