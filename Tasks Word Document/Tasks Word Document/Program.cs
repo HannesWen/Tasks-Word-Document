7a54f8c2a451fd3494cf0ef87987f5e63a096f64
Wengert, Johannes <johannes.wengert@tecalliance.net>
2022-09-20 09:38:51 +0200

    First Commit

---
+++ Tasks Word Document/Tasks Word Document/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tasks_Word_Document
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //
+        }
+    }
+}
