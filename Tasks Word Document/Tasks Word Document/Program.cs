d4d49f84b76eb5ca51f931b468ba961b6cfc22b4
Wengert, Johannes <johannes.wengert@tecalliance.net>
2022-09-20 09:44:59 +0200

    //Seite 1, Aufgabe 1: initialisiere 2 Zahlen und gebe die Summe der Zahlen auf der Console aus

--- Tasks Word Document/Tasks Word Document/Program.cs
+++ Tasks Word Document/Tasks Word Document/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,7 +11,14 @@ namespace Tasks_Word_Document
     {
         static void Main(string[] args)
         {
-            //
+            //o	Aufgabe 1: initialisiere 2 Zahlen und gebe die Summe der Zahlen auf der Console aus
+            int zahl1 = 1;
+            int zahl2 = 2;
+            Console.WriteLine($"Die Summe der Zahlen ist {zahl1 + zahl2}");
+            Console.ReadKey();
+
+
+
         }
     }
 }
